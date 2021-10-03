47b5eb68f4cb889e36fe47db49843ef0c59bb19d
manuel <manuel.delverme@gmail.com>
2021-10-03 14:01:52 +0200

    [CLUSTER] OAM

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,9 @@
-try:
-    from .utils import ddict, get_devices
-    from .datasets import load_dataset
-
-except ImportError as e:
-    import sys
-    print('''Could not import submodules (exact error was: %s).''' % e, file=sys.stderr)
+# try:
+from .utils import ddict, get_devices
+from .datasets import load_dataset
+# except ImportError as e:
+#     import sys
+#     print('''Could not import submodules (exact error was: %s).''' % e, file=sys.stderr)
 
 
 __all__ = [
